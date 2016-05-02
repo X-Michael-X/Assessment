424bb1b283bb0927549d6a7d0849010ca6521669
X-Michael-X <mdporter83@gmail.com>
2016-05-02 11:22:48 -0500

    Update chapter_one_exercises.rb

--- chapter_one_exercises.rb
+++ chapter_one_exercises.rb
@@ -10,10 +10,12 @@
 
 # Exercise 3
     movies = { jaws: 1975,
-                 anchorman: 2004,
-                   man_of_steel: 2013,
-                     a_beautiful_mind: 2001,
-                       the_evil_dead: 1981, }
+               anchorman: 2004,
+               man_of_steel: 2013,
+               a_beautiful_mind: 2001,
+               the_evil_dead: 1981, 
+       
+    }
     puts movies[:jaws]
       puts movies[:anchorman]
         puts movies[:man_of_steel]
