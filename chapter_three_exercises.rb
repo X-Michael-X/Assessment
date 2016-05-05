89e4967e009becc04a9c80dd56d3560817034044
Michael Porter <Mdporter83@gmail.com>
2016-05-05 21:10:30 +0000

    chapter three exercises

---
+++ chapter_three_exercises.rb
@@ -0,0 +1,41 @@
+# Exercise 1
+
+def greeting(name)
+  puts "Hello " + name
+end
+
+puts greeting("Michael")
+
+# Exercise 2
+1.2
+2.nil
+3."Joe"
+4."four"
+5.nil
+
+# Exercise 3
+
+def multiply(n1, n2)
+  n1 * n2
+end
+
+multiply(4, 5)
+
+# Exercise 4
+
+"This exercise returns nothing"
+
+# Exercise 5
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippee")
+
+"The return is still nil"
+
+# Exercise 6
+
+"You are calling a method called calculate_product , but you are only passing it one argument instead of two."
