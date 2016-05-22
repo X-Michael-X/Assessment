53adda1aff074caf282377300dd775a5e21f90ff
Michael Porter <Mdporter83@gmail.com>
2016-05-22 17:50:39 +0000

    movie generator

---
+++ movie.rb
@@ -0,0 +1,8 @@
+subject = ["Two brothers", "A boy and his dog", "Some aliens", "Household pets", "Twin sisters", "Three dirty hobos"]
+locale = ["Paris", "some ancient ruins", "back in time", "rural America","into the future", "to an alien world",]
+quest = ["to find love", "to fight evil", "to steal something important", "to save a friend", "to become the best rapper" ]
+complication = ["one of them gets possessed", "one is actually a robot", "one is dying", "an alien armada attacks Earth", "they fall in love"]
+
+moviegen = puts "#{subject.sample} go to #{locale.sample} #{quest.sample} but #{complication.sample}."
+
+moviegen
